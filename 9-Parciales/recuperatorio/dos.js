dfd7e44b249148db9f0627e0ccdadf36c828fbaf
gastonlfnt <gastonflafuente@gmail.com>
2018-02-06 12:28:53 -0300

    Ejercicio Recuperatorio II finalizado

--- 9-Parciales/recuperatorio/dos.js
+++ 9-Parciales/recuperatorio/dos.js
@@ -1,4 +1,10 @@
 function Mostrar()
-{
+{  
+    var importe;
+    var importefinal;
+    importe=document.getElementById("importeFinal").value;
+    importe=parseInt(importe);
+    importefinal= importe * 0.25 - importe;
+    alert ("el importe final del producto es... " + importefinal);
   
 }
--- 9-Parciales/recuperatorio/tres.js
+++ 9-Parciales/recuperatorio/tres.js
@@ -1,4 +1,20 @@
 function Mostrar()
 {
+    var precio1;
+    var precio2;
+    var precio3;
+    var suma;
+    var promedio;
+    precio1=document.getElementById("precioUno").value;        
+    precio2=document.getElementById("precioDos").value;
+    precio3=document.getElementById("precioTres").value;
+    precio1=parseInt(precio1);
+    precio2=parseInt(precio2);
+    precio3=parseInt(precio3);
+    suma= precio1 + precio2 + precio3;
+    alert ("la suma de la compra es " + suma + " pesos");
+    promedio= suma / 3;
+    alert ("el promedio es " + promedio);
+
 
 }
