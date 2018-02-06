8aacba1abe7b46082a66ac442e424461c2911532
gastonlfnt <gastonflafuente@gmail.com>
2018-02-06 11:23:58 -0300

    ejercicio 7 termindo, parcial - ejercicio uno terminado.

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -3,6 +3,15 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
-{
+{   
+    var dividendo;
+    var divisor;
+    var resto;
+    dividendo=document.getElementById("dividendo").value;
+    divisor=document.getElementById("divisor").value;
+    dividendo=parseInt(dividendo);
+    divisor=parseInt(divisor);
+    resto= diviendo % divisor;
+    alert ("el resto de la división es " + resto);
 	
 }
--- 9-Parciales/parcial/uno.js
+++ 9-Parciales/parcial/uno.js
@@ -1,5 +1,15 @@
 
 function Mostrar()
 {
+    var base;
+    var suma;
+    base=document.getElementById("laBase").value;
+    base=parseInt(base);
+    suma= base + base + base + base;
+    alert ("el perimetro de su cuadrado es de... " + suma);
+
+
+
+
 
 }
