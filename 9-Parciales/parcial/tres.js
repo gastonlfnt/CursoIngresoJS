e337f2b9e599c2676a41c2402c946d37bb525819
gastonlfnt <gastonflafuente@gmail.com>
2018-02-06 11:57:33 -0300

    Parcial UNO Ejercicio número III

--- 9-Parciales/parcial/tres.js
+++ 9-Parciales/parcial/tres.js
@@ -1,4 +1,14 @@
 function Mostrar()
 {
+    var largo;
+    var ancho;
+    var perimetro;
+    largo=document.getElementById("alrgo").value;
+    ancho=document.getElementById("ancho").value;
+    largo=parseInt(largo);
+    ancho=parseInt(ancho);
+    perimetro= (largo * 2 + ancho * 2)*3;
+    alert ("se necesita " + perimetro + " metros de alambre");
+
 
 }
