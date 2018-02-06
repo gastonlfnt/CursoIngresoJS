8d29cab03b097dd411a8819a736236da1ec4b10c
gastonlfnt <gastonflafuente@gmail.com>
2018-02-06 09:28:28 -0300

    Ejercicio 3 arreglado.

--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -3,8 +3,8 @@ y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
     var dato;
-    dato=document.getElementById("elNombre").value;
-    alert ("dato");
+    dato = document.getElementById("elNombre").value;
+    alert ("dato "+ dato) ;
 
 	
 }
