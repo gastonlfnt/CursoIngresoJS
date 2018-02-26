69587d5d166cccbb2d3f9cb4af56786491d5b4aa
gastonlfnt <gastonflafuente@gmail.com>
2018-02-26 11:59:48 -0300

    TP NUMERO 8 - LEGAJOS

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -9,5 +9,114 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
+    var edad = document.getElementById("Edad").value;
+    var sexo = document.getElementById("Sexo").value;
+    var estadoCivil = document.getElementById("EstadoCivil").value;
+    var sueldoBruto = document.getElementById("Sueldo").value
+    var numeroDeLegajo;
+    var nacionalidad;
+  {  edad= prompt ("Ingrese su edad " )
+  //INGRESO DE EDAD// 
+     
+     do 
+      {
+         console.log (edad)
+         alert ("Su edad es " + edad)
+
+         break;
+         //SI CUMPLE LOS REQUISITOS, SIGUE.//
+
+     }
+     
+     while (edad>=18&&edad<=90)
+//SI ESTÀ ENTRE 18 Y 90 AÑOS//
+    
+
+      if (edad>90)  // SI LA EDAD ES MAYOR A 90...//
+     {
+         alert ("Usted sobrepaso la edad");
+         
+     }
+
+     else if   (edad<17) 
+     //SI LA EDAD ES MENOR A 18...//    
+     {
+         alert ("Usted es menor de edad")
+     }
+}
+
+    
+  {  while (sexo!="f" && sexo!="m") //Si es F = FEMENINO M Para MASCULINO//
+
+    {   sexo =  prompt ("Ingrese 'M' para masculino ó 'F' para femenino")
+
+        if (sexo=="m")
+       { alert ("Su sexo es 'Masculino'")
+       console.log (sexo)
+       break; //Si cumple se corta y pasamos al siguiente//
+    }
+        
+
+        else  (sexo=="f")
+   alert ("Su sexo es 'Femenino'");
+   break;
+}}
+
+     {
+        
+{
+    var estadoCivil=document.getElementById("EstadoCivil").value; 
+    console.log (estadoCivil)
+    estadoCivil= prompt ("ESTADO CIVIL 1-SOLTERO, 2- CASADO, 3-DIVORCIADO, 4-VIUDO."); 
+    switch (estadoCivil) 
+   {   
+
+       case "1":
+       alert ("Su Estado Civil : SOLTERO");
+       break;
+
+       case "2":
+       alert ("Su Estado Civil : CASADO");
+       break;
+
+       case "3":
+       alert ("Su Estado Civil : DIVORCIADO");
+       break;
+
+       case "4":
+       alert ("Su Estado Civil : VIUDO");
+       break;
+   }
+    
+
+  {     
+      sueldoBruto = prompt ("Ingrese Su Sueldo Bruto");
+        
+        while (sueldoBruto>8000)
+        console.log (sueldoBruto);{
+            
+        if (sueldoBruto>=8000)
+
+        {
+            alert ("Su sueldo es. " + sueldoBruto)
+        }
+
+        else
+        {
+         alert ("Su sueldo es menor al permitido.")   
+        }
+
+    
+}
+}
+
+}
+    
+
+    
+
+
+    
  
+}
 }
